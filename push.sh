cb5b7a99ba089038dd8187d07286b48a19b70eda
marcin_laskawski <laskawski.marcin@gmail.com>
2020-07-12 11:29:51 +0200

    fix url in scripts - point to proper project

--- push.sh
+++ push.sh
@@ -5,5 +5,5 @@ branch="$2"
 git push ${remote} ${branch}
 if [ $? -eq 0 ]; then
   credentials="${jenkins_automation_credentials}"
-  curl -X POST http://${credentials}@localhost:8081/job/ftp-client/build?token=86aa139d-bb3f-435f-8eac-62594b34c72a
+  curl -X POST http://${credentials}@localhost:8081/job/spanish-learning/build?token=86aa139d-bb3f-435f-8eac-62594b34c72a
 fi
